d67696b09de1e1bbdba784de2849d12c576f876d
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-06-13 11:03:16 +0200

    Create class for the App

--- GUI/pneumologos.py
+++ GUI/pneumologos.py
@@ -2,3 +2,9 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from pneumologos_ui import Ui_MainWindow
 from fastai.vision.all import *
+
+class PneumologosApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
