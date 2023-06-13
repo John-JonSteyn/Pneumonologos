6e1937b9ceeeec7def21eb8e80b08d1feb2b8968
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-06-13 11:08:12 +0200

    Implement Buttons
    + Implement logic for the xray button.
    + Implement Logic for the Analyse bytton.

--- GUI/pneumologos.py
+++ GUI/pneumologos.py
@@ -10,10 +10,24 @@ class PneumologosApp(QMainWindow):
         self.ui.setupUi(self)
 
     def upload_xray(self):
-        pass
+       options = QFileDialog.Options()
+       options |= QFileDialog.DontUseNativeDialog
+       file_name, _ = QFileDialog.getOpenFileName(self, "Upload X-ray", "", "Image Files (*.png *.jpg *.jpeg)", options=options)
+       if file_name:
+           pixmap = QPixmap(file_name)
+           self.ui.label.setPixmap(pixmap.scaled(691, 691, Qt.AspectRatioMode.KeepAspectRatio))
 
     def analyse_xray(self):
-        pass
+       pixmap = self.ui.label.pixmap()
+       if pixmap:
+           img = PILImage.fromqpixmap(pixmap)
+           pred, _, probs = self.learn.predict(img)
+           self.ui.labelDiagnosis.setText(f"Diagnosis: {pred}")
+           self.ui.lcdNumberProbability.display(probs[1])
+       else:
+           self.ui.labelDiagnosis.setText("Please upload an X-ray first.")
+           self.ui.lcdNumberProbability.display(0.0)
+
 
     if __name__ == "__main__":
         app = QApplication(sys.argv)
