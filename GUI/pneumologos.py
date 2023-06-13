717a41c4fa9f61347a324fcf9887a61e577892a6
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-06-13 13:26:27 +0200

    Implement Upload Functionality
    + Display the file dialog window.
    + Trigger `process_selected_file` method.
    + Check if a file name is provided. If so, loads the selected image file using `QPixmap` and display it in the `labelXRayDisplay` widget.
    + Set a text to show if no file is selected.
    + Calculates the total size of the selected file and sets a chunk size for processing the file in chunks.
    + Open the selected file in binary read mode and iteratively reads chunks of data.
    + Update the `progressBarUpload` widget by calculating the percentage of uploaded data based on the total file size for each chunk read.

--- GUI/pneumologos.py
+++ GUI/pneumologos.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
@@ -12,12 +13,40 @@ class PneumologosApp(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButtonUpload.clicked.connect(self.upload_xray)
 
     def upload_xray(self):
-       pass
+        options = QFileDialog.Option(QFileDialog.Option.ReadOnly | QFileDialog.Option.DontUseNativeDialog)
+        file_dialog = QFileDialog(self, "Upload X-ray", "", "JPEG Files (*.jpeg *.jpg);;All Files (*)", options=QFileDialog.Option(options))
+        file_dialog.fileSelected.connect(self.process_selected_file)
+        file_dialog.show()
+
+    def process_selected_file(self, file_name):
+        if file_name:
+            pixmap = QPixmap(file_name)
+            self.ui.labelXRayDisplay.setPixmap(
+                pixmap.scaled(691, 691, Qt.AspectRatioMode.KeepAspectRatio)
+            )
+        else:
+            self.ui.labelXRayDisplay.setText("No image selected.")
+
+        # Update progress bar
+        if file_name:
+            total_size = os.path.getsize(file_name)
+            chunk_size = 1024 * 1024  # 1 MB
+            with open(file_name, "rb") as file:
+                uploaded_size = 0
+                while True:
+                    data = file.read(chunk_size)
+                    if not data:
+                        break
+                    # Process the chunk (e.g., upload to a server)
+                    uploaded_size += len(data)
+                    progress = int(uploaded_size / total_size * 100)
+                    self.ui.progressBarUpload.setValue(progress)
 
     def analyse_xray(self):
-       pass
+        pass
 
 
 if __name__ == "__main__":
